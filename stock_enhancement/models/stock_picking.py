777ace04e626dcf1b94ee231430f8eba8b08ec67
powerfulman86 <dba.ahmedmaher@gmail.com>
2025-06-19 10:20:13 +0300

    enhancement manifest

--- stock_enhancement/models/stock_picking.py
+++ stock_enhancement/models/stock_picking.py
@@ -11,6 +11,6 @@ class StockPicking(models.Model):
     def _compute_scheduled_time_only(self):
         for record in self:
             if record.scheduled_date:
-                record.scheduled_time_only = record.scheduled_date.strftime('%H:%M:%S')
+                record.scheduled_time = record.scheduled_date.strftime('%H:%M:%S')
             else:
-                record.scheduled_time_only = ''
+                record.scheduled_time = ''

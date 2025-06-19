0d1ca4bb58c93f92890f2e7b2aa5cb5f180e649a
powerfulman86 <dba.ahmedmaher@gmail.com>
2025-06-19 10:27:19 +0300

    schedule date extract

--- stock_enhancement/models/stock_picking.py
+++ stock_enhancement/models/stock_picking.py
@@ -11,6 +11,6 @@ class StockPicking(models.Model):
     def _compute_scheduled_time_only(self):
         for record in self:
             if record.scheduled_date:
-                record.scheduled_time = record.scheduled_date.strftime('%H:%M:%S')
+                record.scheduled_time = fields.Datetime.to_string(record.scheduled_date)[11:19]
             else:
                 record.scheduled_time = ''
